31894c716f75b033270e67c604777d909f4aed6d
Khwarz <karoumem01@gmail.com>
2024-07-11 13:36:59 +0100

    feat: update configuration

--- Scripts/bluetooth.sh
+++ Scripts/bluetooth.sh
@@ -3,5 +3,5 @@
 
 bluetoothctl agent on >> /dev/null
 bluetoothctl power on >> /dev/null
-result=$(bluetoothctl devices | cut -d\  -f2- | rofi -dmenu -theme $HOME/.config/rofi/themes/appsmenu.rasi | awk '{print $1}')
+result=$(bluetoothctl devices | cut -d\  -f2- | dmenu | awk '{print $1}')
 notify-send "$(bluetoothctl connect $result)"
